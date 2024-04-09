ff9d4afc03ca5f6e7b1521ff928dd281b8528f8f
Cedric002 <cedgnleblanc@gmail.com>
2024-04-09 14:52:24 +0200

    update number-template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -26,7 +26,7 @@ def number_n(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template_n(n):
-    return render_template('number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
