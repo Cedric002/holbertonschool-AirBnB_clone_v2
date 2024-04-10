efef2fd3ea82fe514d1db4bc634b3d922b922ebc
Cedric002 <cedgnleblanc@gmail.com>
2024-04-10 09:45:14 +0200

    fix 2 number-template and number-html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -26,11 +26,7 @@ def number_route(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    try:
-        n = int(n)
-        return render_template('number.html', n=n)
-    except ValueError:
-        return render_template('number.html', n="not a number")
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
