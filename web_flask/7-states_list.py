0a65224e040d488ed87198b268e2ec858a7c23d3
Cedric002 <cedgnleblanc@gmail.com>
2024-04-10 14:59:42 +0200

    add state-list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,22 @@
-d
+#!/usr/bin/python3
+"With Flask web application, display route for 'State_list'"
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/states_list')
+def states_list():
+    states = storage.all("State").values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
