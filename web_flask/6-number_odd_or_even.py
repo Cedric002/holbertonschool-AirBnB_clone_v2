22e24066f1cce66007eaafe40ede555a149d288e
Cedric002 <cedgnleblanc@gmail.com>
2024-04-10 10:31:47 +0200

    fix 2 number-odd-or-even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -30,10 +30,7 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    if n % 2 == 0:
-        return render_template('number_odd_or_even.html', n=n, is_even=True)
-    else:
-        return render_template('number_odd_or_even.html', n=n, is_even=False)
+    return render_template('number_odd_or_even.html', n=n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
