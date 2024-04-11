505606388d2a1adfc6d7a9848aaa3b300ab65417
Cedric002 <cedgnleblanc@gmail.com>
2024-04-11 15:12:24 +0200

    fix states-py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"With Flask web application, display route for 'cities_by_states'"
+"With Flask web application, display route for 'states'"
 
 from flask import Flask, render_template
 from models import storage
@@ -9,21 +9,13 @@ app.url_map.strict_slashes = False
 
 @app.route('/cities_by_states')
 def cities_by_states():
-    states = storage.all("State").values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('9-states.html', states=states)
-
-@app.route('/states/<id>')
-def state_cities(id):
-    state = storage.get('State', id)
-    if state:
-        return render_template('9-states.html', state=state)
-    else:
-        return render_template('9-states.html', state=None)
+    states = sorted(storage.all("State").values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
 
 @app.teardown_appcontext
-def remove_session(exception=None):
+def teardown_db(exception):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
