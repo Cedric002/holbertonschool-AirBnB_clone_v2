468377944f58ca99be458fdb35a5bafc41441c1f
Cedric002 <cedgnleblanc@gmail.com>
2024-04-11 15:44:17 +0200

    pep8 cities-by-states-py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,11 +7,13 @@ from models import storage
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/cities_by_states')
 def cities_by_states():
     states = storage.all("State").values()
     states = sorted(states, key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states=states)
+
 
 @app.teardown_appcontext
 def remove_session(exception=None):
