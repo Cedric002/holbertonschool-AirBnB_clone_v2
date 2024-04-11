b9e36a0fa6d26007834ebf17e62ff201bc10fa01
Cedric002 <cedgnleblanc@gmail.com>
2024-04-11 11:34:15 +0200

    update 8-cities_by_states-py; add 9-statse-py and 9--html

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,12 +11,19 @@ app.url_map.strict_slashes = False
 def cities_by_states():
     states = storage.all("State").values()
     states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=states)
+    return render_template('9-states.html', states=states)
+
+@app.route('/states/<id>')
+def state_cities(id):
+    state = storage.get('State', id)
+    if state:
+        return render_template('9-states.html', state=state)
+    else:
+        return render_template('9-states.html', state=None)
 
 @app.teardown_appcontext
 def remove_session(exception=None):
     storage.close()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
