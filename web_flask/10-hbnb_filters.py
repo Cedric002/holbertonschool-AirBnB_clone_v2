4dae4caadf30c2a5c2830be56c3ff9a4f5fca13b
Cedric002 <cedgnleblanc@gmail.com>
2024-04-12 17:33:50 +0200

    update hbnb-filter-py and hbnb-filter-html

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application."""
+"With Flask web application, display route for HBNB web static"
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
-from flask import Flask, render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
+@app.route('/hbnb_filters', strict_slashes=False)
 def display_html():
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
     return render_template(
         "10-hbnb_filters.html", states=states, amenities=amenities)
 
