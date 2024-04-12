7ea8c65a17a16d2676d3378939fc2c075b07a078
Cedric002 <cedgnleblanc@gmail.com>
2024-04-12 15:44:39 +0200

    fix states-py and state-html

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,13 +11,13 @@ app = Flask(__name__)
 @app.route('/states', strict_slashes = False)
 def states():
     states = storage.all(State)
-    return render_template("9-states.html", state=states)
+    return render_template("9-states.html", states=states)
 
 
 @app.route('/states/<id>', strict_slashes = False)
 def state(id):
     for state in storage.all(State).values():
-        if state == id:
+        if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
